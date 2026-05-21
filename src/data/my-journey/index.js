0ce1c921dc1931adbf8cc8d9fd7d01559a9694f0
ViliusDevX <Viliusnikitinas14@gmail.com>
2026-05-21 20:17:32 +0300

    Add third questionnaire, delete unnecessary js files

--- src/data/my-journey/index.js
+++ src/data/my-journey/index.js
@@ -1,4 +1,5 @@
 import entry001 from "./001_questionnaire.json";
 import entry002 from "./002_questionnaire.json";
+import entry003 from "./003_questionnaire.json";
 
-export const journeyEntries = [entry001, entry002];
+export const journeyEntries = [entry001, entry002, entry003];
