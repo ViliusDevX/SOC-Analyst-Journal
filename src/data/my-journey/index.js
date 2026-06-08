652e2d6d0b32a1cf0585e0f5cdb4a60932f8c00f
ViliusDevX <Viliusnikitinas14@gmail.com>
2026-06-08 19:25:36 +0300

    Add 5th json my journey file

--- src/data/my-journey/index.js
+++ src/data/my-journey/index.js
@@ -2,5 +2,6 @@ import entry001 from "./001_questionnaire.json";
 import entry002 from "./002_questionnaire.json";
 import entry003 from "./003_questionnaire.json";
 import entry004 from "./004_questionnaire.json";
+import entry005 from "./005_questionnaire.json";
 
-export const journeyEntries = [entry001, entry002, entry003, entry004];
+export const journeyEntries = [entry001, entry002, entry003, entry004, entry005];
