d449986b953a193d7ad0b9ced270d01b3ece6a21
ViliusDevX <Viliusnikitinas14@gmail.com>
2026-06-11 15:29:11 +0300

    Add 008 json file and import entry

--- src/data/my-journey/index.js
+++ src/data/my-journey/index.js
@@ -5,5 +5,6 @@ import entry004 from "./004_questionnaire.json";
 import entry005 from "./005_questionnaire.json";
 import entry006 from "./006_questionnaire.json";
 import entry007 from "./007_questionnaire.json";
+import entry008 from "./008_questionnaire.json";
 
-export const journeyEntries = [entry001, entry002, entry003, entry004, entry005, entry006, entry007];
+export const journeyEntries = [entry001, entry002, entry003, entry004, entry005, entry006, entry007, entry008];
