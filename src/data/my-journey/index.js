c9688204febd92272b7e4285bbf2f6c44dd04cfe
ViliusDevX <Viliusnikitinas14@gmail.com>
2026-06-12 11:02:16 +0300

    Add 10th json file and import entry

--- src/data/my-journey/index.js
+++ src/data/my-journey/index.js
@@ -7,5 +7,6 @@ import entry006 from "./006_questionnaire.json";
 import entry007 from "./007_questionnaire.json";
 import entry008 from "./008_questionnaire.json";
 import entry009 from "./009_questionnaire.json";
+import entry010 from "./010_questionnaire.json";
 
-export const journeyEntries = [entry001, entry002, entry003, entry004, entry005, entry006, entry007, entry008, entry009];
+export const journeyEntries = [entry001, entry002, entry003, entry004, entry005, entry006, entry007, entry008, entry009, entry010];
